4f0669bb6708906a32c12e18e1859a853319c3c3
Abed <abed@abeds.fritz.box>
2016-12-13 20:52:33 +0100

    Initial Commit

---
+++ MyCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCal
+//
+//  Created by Abed on 13/12/2016.
+//  Copyright © 2016 Webiaturist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
