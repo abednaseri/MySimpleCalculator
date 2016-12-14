9d3607988c87a01b328133747f0b096ab024ccd2
abednaseri <abednaseri@yahoo.com>
2016-12-14 17:11:41 +0100

    Add files via upload
    This file contains codes I wrote

--- MyCal/ViewController.swift
+++ MyCal/ViewController.swift
@@ -10,16 +10,114 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLbl: UILabel!
+    var leftVal = ""
+    var rightVal = ""
+    var theOperation = ""
+    var runningNumber = ""
+    var result = ""
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equal = "="
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func caclulation(abed_operator: Operation){
+        if currentOperation != Operation.Empty{
+            if runningNumber != ""{
+                rightVal = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Divide{
+                    result = "\(Double(leftVal)! / Double(rightVal)!)"
+                }
+                if currentOperation == Operation.Multiply{
+                    result = "\(Double(leftVal)! * Double(rightVal)!)"
+                }
+                if currentOperation == Operation.Add{
+                    result = "\(Double(leftVal)! + Double(rightVal)!)"
+                }
+                if currentOperation == Operation.Subtract{
+                    result = "\(Double(leftVal)! - Double(rightVal)!)"
+                }
+                
+                leftVal = result
+                resultLbl.text = result
+            }
+            currentOperation = abed_operator
+        } else{
+            leftVal = runningNumber
+            runningNumber = ""
+            currentOperation = abed_operator
+        }
     }
+    
+    @IBAction func onNumberPressed(sender: UIButton){
+        runningNumber += "\(sender.tag)"
+        resultLbl.text = runningNumber
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject){
+        caclulation(abed_operator: Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject){
+        caclulation(abed_operator: .Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject){
+        caclulation(abed_operator: .Subtract)
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject){
+        caclulation(abed_operator: .Add)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject){
+        caclulation(abed_operator: currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject){
+        currentOperation = Operation.Empty
+        leftVal = ""
+        rightVal = ""
+        resultLbl.text = "0"
+    }
+    
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
